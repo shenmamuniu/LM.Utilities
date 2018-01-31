a0c115480f8755fcdfa9743d363d17f3fbaa0837
Lightmao <Lightmao@DESKTOP-LS9K8IP>
2018-01-31 20:54:32 +0800

    Add call cmd,mongodb operator.

---
+++ LM.Utilities/DBAccess/MongoDBHelper.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM.Utilities.DBAccess
+{
+    /// <summary>
+    /// provide operation help on mongodb based on offical mongodb driver 
+    /// </summary>
+    public class MongoDBHelper
+    {
+        IMongoDatabase db = null;
+        /// <summary>
+        /// Initial db object;
+        /// </summary>
+        /// <param name="connectionString">like mongodb://127.0.0.1:27017</param>
+        /// <param name="dbName"></param>
+        public MongoDBHelper(string connectionString,string dbName) {
+            MongoUrl mongourl = MongoUrl.Create(connectionString);
+            var client = new MongoClient(mongourl);
+            db = client.GetDatabase(dbName);
+        }
+    }
+}
---
+++ LM.Utilities/WinTools.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM.Utilities
+{
+    /// <summary>
+    /// It'a about interactive with local system.
+    /// </summary>
+    public class WinTools
+    {
+        /// <summary>
+        /// Exccute commands in cmd
+        /// </summary>
+        /// <param name="inputAction">like p=>{p("");p("");}</param>
+        public static void ExecBatCommand(Action<Action<string>> inputAction) {
+            Process pro = null;
+            StreamWriter sIn = null;
+            StreamReader sOut = null;
+
+            try
+            {
+                pro = new Process();
+                pro.StartInfo.FileName = "cmd.exe";
+                pro.StartInfo.UseShellExecute = false;
+                pro.StartInfo.CreateNoWindow = true;
+                pro.StartInfo.RedirectStandardInput = true;
+                pro.StartInfo.RedirectStandardOutput = true;
+                pro.StartInfo.RedirectStandardError = true;
+
+                pro.OutputDataReceived += (sender, e) => {
+                   
+                };
+                pro.ErrorDataReceived += (sender, e) => {
+                   
+                };
+
+                pro.Start();
+                sIn = pro.StandardInput;
+                sIn.AutoFlush = true;
+
+                pro.BeginOutputReadLine();
+                inputAction(value => sIn.WriteLine(value));
+
+                pro.WaitForExit();
+            }
+            finally
+            {
+                if (pro != null && !pro.HasExited)
+                    pro.Kill();
+
+                if (sIn != null)
+                    sIn.Close();
+                if (sOut != null)
+                    sOut.Close();
+                if (pro != null)
+                    pro.Close();
+            }
+        }
+    }
+}
